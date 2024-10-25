7b4ecee1394b6274ba54187d23713f858c35c16e
Hansaja Sandeepa <76199476+hansajasandeepabadalge@users.noreply.github.com>
2024-10-25 21:27:46 +0530

    Hi

---
+++ New/guess.py
@@ -0,0 +1,23 @@
+"""
+Exerciese Write a program for guess game
+
+generate a random number ask the user to guess if the guess number user input is larger then
+random number tell the user too high if the guess number user input is smaller then random number
+tell the user too low if the user get it correct display you got it correct random number and tell
+after so many guesses (keep count of number of guesses) range for the random number is 1 to 100
+
+"""
+import random
+
+count = 0
+while True:
+    count +=1
+    ran_num = random.randint(1, 100)
+    user_input = int(input("Guess the number: "))
+        if user_input == ran_num:
+            print(f"Yes, You are correct. The correct number is {user_input}.")
+            print(f"You get {count} to guess the correct number.")
+        else:
+            print("Try Again.")
+    else:
+        print("Use only whole number from 1 to 100.")
